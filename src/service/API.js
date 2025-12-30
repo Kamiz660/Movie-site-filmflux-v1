362d26729286e7401c4ddcd24fd19c8a2f73b263
Aswanth D <kamizlayer625@gmail.com>
2025-12-30 20:22:33 +0530

    API update

--- src/service/API.js
+++ src/service/API.js
@@ -1,13 +1,20 @@
-const API_KEY = "b1ad1f21760275710950a4b87e0d5978";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMovie = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data = await response.json()
-    return data.results
-};
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+  )
+
+  const data = await response.json()
+  return data.results
+}
+
 export const searchMovie = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = await response.json()
-    return data.results
-};
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  )
+
+  const data = await response.json()
+  return data.results
+}
